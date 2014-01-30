012e934a14b7075d1d2943b7905f8d47dff9628c
StefanTomesen <stefantomesen@gmail.com>
2014-01-30 14:36:03 +0100

    Fixed placeholder buttons

--- Fallen Stars/Button.cpp
+++ Fallen Stars/Button.cpp
@@ -1,10 +1,16 @@
 #include "Button.h"
 
-Button::Button()
+Button::Button(sf::Vector2f position):
+	mPosition(position)
 {
 }
 
 
 Button::~Button()
 {
+}
+
+void Button::render(sf::RenderTarget& renderSurface)
+{
+	// TODO do rendering
 }
--- Fallen Stars/Button.h
+++ Fallen Stars/Button.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+// TODO Implement buttons
 
 class Button
 {
 	public:
+		Button(sf::Vector2f position);
 		~Button();
-
+		void render(sf::RenderTarget& renderSurface);
 
 	private:
-		sf::Vector2f position;
+		sf::Vector2f mPosition;
 };
