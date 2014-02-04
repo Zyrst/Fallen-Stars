ec4ce6929bba4410d6f19723e599ec41a65c8cc2
Zyrst <zajreg@gmail.com>
2014-02-04 13:49:23 +0100

    Fun time error time, gitignore with more paths. Worked on diffrent layer draws.

--- Fallen Stars/LevelManager.cpp
+++ Fallen Stars/LevelManager.cpp
@@ -16,12 +16,15 @@ LevelManager::~LevelManager()
 
 void LevelManager::Load()
 {
-	
 	mapLoader.Load(mLevel + ".tmx");
-	
 }
 
 void LevelManager::Render(sf::RenderTarget& rendertarget)
 {
-	rendertarget.draw(mapLoader);
+	
+			//mapLoader.Draw(rendertarget,tmx::MapLayer::Background);
+			//mapLoader.Draw(rendertarget,tmx::MapLayer::Foreground);
+			mapLoader.Draw(rendertarget,tmx::MapLayer::Object);
+
+	//rendertarget.draw(mapLoader);
 }
