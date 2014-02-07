732d213d2861dba5d027d7f08dbb70773dfbf119
StefanTomesen <stefantomesen@gmail.com>
2014-02-07 15:04:45 +0100

    Added generating mirrored frames to the spritesheet.

--- Fallen Stars/SpriteSheet.cpp
+++ Fallen Stars/SpriteSheet.cpp
@@ -21,17 +21,37 @@ sf::IntRect SpriteSheet::getFrame(int index)
 	return sf::IntRect(xIndex * mSpriteSize.x, yIndex * mSpriteSize.y, mSpriteSize.x, mSpriteSize.y);
 }
 
-std::vector<sf::IntRect> SpriteSheet::getFrames(int startIndex, int frameCount)
+sf::IntRect SpriteSheet::getMirroredFrame(int index)
+{
+	assert(0 < index && index < getFrameCount());
+	int xIndex = index % mTiles.x;
+	int yIndex = index / mTiles.x;
+	return sf::IntRect(xIndex * mSpriteSize.x +mSpriteSize.x, yIndex * mSpriteSize.y, -mSpriteSize.x, mSpriteSize.y);
+}
+
+std::vector<sf::IntRect> SpriteSheet::getFrames(int startIndex, int frameCount, bool mirrored)
 {
 	std::vector<sf::IntRect> frames(frameCount);
 	for(int i=0; i<frameCount; i++)
 	{
-		frames.push_back(getFrame(startIndex + i));
+		frames.push_back(getFrame(startIndex + i, mirrored));
 	}
 	return frames;
 }
 
-std::vector<sf::IntRect> SpriteSheet::getAllFrames()
+std::vector<sf::IntRect> SpriteSheet::getAllFrames(bool mirrored)
 {
-	return getFrames(0, getFrameCount());
+	return getFrames(0, getFrameCount(), mirrored);
 }
+
+sf::IntRect SpriteSheet::getFrame(int index, bool mirrored)
+{
+	if(!mirrored)
+	{
+		return getFrame(index);
+	}
+	else
+	{
+		return getMirroredFrame(index);
+	}
+};
