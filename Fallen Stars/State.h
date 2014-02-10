3bb41c52c5bead598eb540c5d5fe123653c40782
StefanTomesen <stefantomesen@gmail.com>
2014-02-10 12:09:29 +0100

    Added a resource collection to the states as a member variable

--- Fallen Stars/State.h
+++ Fallen Stars/State.h
@@ -3,6 +3,7 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include "Controls.h"
+#include "ResourceCollection.h"
 
 /* States represent the various different sections of the game. The state contatins the environment that the
  * game currently uses. The active state also takes care of all the rendering performed during the current
@@ -17,4 +18,7 @@ class State
 		virtual void update(sf::Time deltaTime) = 0;
 		virtual void render(sf::RenderWindow& window) = 0;
 		virtual void handleAction(Controls::Action action, Controls::KeyState) = 0;
+
+	private:
+		ResourceCollection mResourceCollection;
 };
