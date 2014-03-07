795c26dbec26806b628e21af22450ff7eafe271d
stefantomesen <stefantomesen@gmail.com>
2014-03-07 14:28:44 +0100

    Fix for the exit crash! :D

--- Fallen Stars/Main.cpp
+++ Fallen Stars/Main.cpp
@@ -1,7 +1,19 @@
 #include "Game.h"
 
+#ifdef _WIN32
+#ifdef _DEBUG
+#include <Windows.h>
+#endif
+#endif
+
 void main()
 {
 	Game game;
 	game.run();
+
+	#ifdef _WIN32
+	#ifdef _DEBUG
+		TerminateProcess(GetCurrentProcess(), EXIT_SUCCESS);
+	#endif
+	#endif
 }
